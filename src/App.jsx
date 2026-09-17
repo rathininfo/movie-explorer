7d8c74f06d3655b1f98b8a0d0a840118153ed37a
Rathin <rathininfo7@gmail.com>
2026-09-17 19:28:20 +0600

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { RouterProvider } from "react-router/dom";
+import { createBrowserRouter } from "react-router";
 
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-4xl font-bold">Hello World</div>,
+  },
+]);
 
+function Router() {
   return (
-    <>
-     <div>Movie Explorar</div>
-    </>
-  )
+      <RouterProvider router={router} /> 
+    )
+    
 }
 
-export default App
+export default Router; 
